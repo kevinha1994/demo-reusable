a1cf06ba7a83d8515a9608d1ca30fad1860746a8
Kevin Hassw <hasswkevin@hotmail.de>
2025-12-11 19:28:47 +0100

    Enhance test_ping_service with status code output
    Added print statement to confirm assertion test result.

--- tests/e2e/test_e2e_example.py
+++ tests/e2e/test_e2e_example.py
@@ -3,3 +3,4 @@ import requests
 def test_ping_service():
     resp = requests.get("https://github.com/kevinha1994/demo-reusable")
     assert (resp.status_code == 200) or (resp.status_code == 504)
+    print(f"Successfully passed assertion test: status_code = {resp.status_code}")
