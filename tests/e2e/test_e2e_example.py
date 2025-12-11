db1755fcd8b4161d943179136b5503b66d46a351
Kevin Hassw <hasswkevin@hotmail.de>
2025-12-11 19:41:19 +0100

    Remove direct call to test_ping_service
    Remove the call to test_ping_service() at the end of the file.

--- tests/e2e/test_e2e_example.py
+++ tests/e2e/test_e2e_example.py
@@ -4,5 +4,3 @@ def test_ping_service():
     resp = requests.get("https://github.com/kevinha1994/demo-reusable")
     assert (resp.status_code == 200) or (resp.status_code == 504)
     print(f"Successfully passed assertion test: status_code = {resp.status_code}")
-
-test_ping_service()
